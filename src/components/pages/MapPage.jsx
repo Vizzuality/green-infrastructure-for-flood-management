f60d305da02e40abb01337d1d1af36f2e4b29d9a
bitTal <albafjez@gmail.com>
2017-04-03 18:14:58 +0200

    Filter/search by name

--- src/components/pages/MapPage.jsx
+++ src/components/pages/MapPage.jsx
@@ -67,7 +67,7 @@ export default class MapPage extends React.Component {
   }
 
   onSearchChange(val) {
-    this.props.setProjectsFilters({ search: val });
+    this.props.setProjectsFilters({ name: val });
   }
 
   goToProjectDetail(projectId) {
@@ -227,7 +227,7 @@ export default class MapPage extends React.Component {
                 <Filters />
               </SlidingMenu>
               <Search
-                defaultValue={this.props.filters.search}
+                defaultValue={this.props.filters.name}
                 onChange={evt => this.onSearchChange(evt.target.value)}
               />
               <ProjectList projects={this.props.projects} onProjectSelect={this.goToProjectDetail} />
