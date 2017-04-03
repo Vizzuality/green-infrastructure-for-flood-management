538fa7261dd41ef784321b6a0f1840c6d4927cc9
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-04-03 15:47:30 +0200

    Footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,27 +2,9 @@ import { Link } from 'react-router';
 import React from 'react';
 import Nav from 'components/ui/Nav';
 import classnames from 'classnames';
+import links from 'constants/links';
 
 export default function Header() {
-  const links = [
-    {
-      text: 'Map of global projects',
-      href: '/map'
-    },
-    {
-      text: 'Resources',
-      href: '/resources'
-    },
-    {
-      text: 'Submit',
-      href: '/submit'
-    },
-    {
-      text: 'About',
-      href: '/about'
-    }
-  ];
-
   const cNames = {
     header: classnames('c-header', { '-home': window.location.pathname === '/' }),
     nav: classnames('header-nav', { '-home': window.location.pathname === '/' })
