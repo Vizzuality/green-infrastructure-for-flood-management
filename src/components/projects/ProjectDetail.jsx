67986653bf422621ba8f2179722086e0eb13ba0b
bitTal <albafjez@gmail.com>
2017-04-04 16:23:43 +0200

    Detail data order

--- src/components/projects/ProjectDetail.jsx
+++ src/components/projects/ProjectDetail.jsx
@@ -4,6 +4,7 @@ import { Row } from 'components/ui/Grid';
 
 export default function ProjectDetail(props) {
   const { data } = props;
+  const setArrayValues = array => array.map((pboi, i) => <span className="value-item" key={i}>{pboi.name}</span>);
 
   return (
     <article className="c-project-detail">
@@ -21,16 +22,14 @@ export default function ProjectDetail(props) {
       <div className="project-resumme">
         <div className="project-cost">
           <Row>
-            <div className="small-6">
-              <span className="label">Monetary cost ( us$ Mildivons)</span>
-              <span className="value -big">{data.estimated_cost}</span>
-            </div>
-            <div className="small-6">
-              <span className="label">Monetary Benefits</span>
-              <span className="value -big">{data.estimated_monetary_benefits}</span>
+            <div className="small-12">
+              <span className="label">Nature based solutions</span>
+              <span className="value -big">{setArrayValues(data.nature_based_solutions)}</span>
             </div>
           </Row>
         </div>
+
+        <span className="label">Project resume</span>
         <p className="project-text">{data.summary}</p>
         <a className="project-link" rel="noopener noreferrer" target="_blank" href="http://www.worldbank.org/">website</a>
       </div>
@@ -46,25 +45,55 @@ export default function ProjectDetail(props) {
               <span className="value">{data.hazard_types.map((ht, i) => <span className="value-item" key={i}>{ht.name}</span>)}</span>
             </div>
             <div className="small-4">
-              <span className="label">Main donor</span>
-              <span className="value">aaa</span>
+              <span className="label">Scale</span>
+              <span className="value">{data.scale}</span>
             </div>
           </Row>
         </div>
-        <div className="project-info-item">
-          <span className="label">Benefits detail</span>
-          <span className="value">{data.primary_benefits_of_interventions.map((pboi, i) => <span className="value-item" key={i}>{pboi.name}</span>)}</span>
-        </div>
 
-        <div className="project-info-item">
+        {data.primary_benefits_of_interventions.length && <div className="project-info-item">
+          <span className="label">Primary benefits of intervention</span>
+          <span className="value">{setArrayValues(data.primary_benefits_of_interventions)}</span>
+        </div>}
+
+        {data.co_benefits_of_interventions.length && <div className="project-info-item">
           <span className="label">Co-benefits of intervention</span>
-          <span className="value">aaa</span>
+          <span className="value">{setArrayValues(data.co_benefits_of_interventions)}</span>
+        </div>}
+        
+        <div className="project-info-item">
+          <span className="label">Main Donor</span>
+          <span className="value">{data.donors.length ? data.donor[0].name : 'Unknown'}</span>
         </div>
 
         <div className="project-info-item">
-          <span className="label">Natured-based solution</span>
-          <span className="value">{data.nature_based_solutions.map((nbs, i) => <span className="value-item" key={i}>{nbs.name}</span>)}</span>
+          <Row>
+            <div className="small-6">
+              <span className="label">Est. Monetary Cost (Today's US$)</span>
+              <span className="value">{data.estimated_cost} US$</span>
+            </div>
+            <div className="small-6">
+              <span className="label">Est. Monetary benefits</span>
+              <span className="value">{data.estimated_monetary_benefits} US$</span>
+            </div>
+          </Row>
         </div>
+
+        {data.benefit_details && <div className="project-info-item">
+          <span className="label">Benefits detail</span>
+          <span className="value">{data.benefit_details}</span>
+        </div>}
+
+        {data.learn_more !== '' && <div className="project-info-item">
+          <span className="label">Learn more</span>
+          <span className="value">{data.learn_more}</span>
+        </div>}
+
+        {data.references !== '' && <div className="project-info-item">
+          <span className="label">References</span>
+          <span className="value">{data.references}</span>
+        </div>}
+
 
       </div>
     </article>
