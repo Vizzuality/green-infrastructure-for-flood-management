6d2d37e9c6935c4a16b2250aad4fab890b2d88c6
bitTal <albafjez@gmail.com>
2017-04-27 12:55:17 +0200

    Project list fits & add country

--- src/components/projects/ProjectItem.jsx
+++ src/components/projects/ProjectItem.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import upperFirst from 'lodash/upperFirst';
+import uniq from 'lodash/uniq';
 import { SvgIcon } from 'vizz-components';
 import PlusNumber from 'components/ui/PlusNumber';
 import { Link } from 'react-router';
 
 export default function ProjectItem(props) {
   const data = props.data;
+  const countries = uniq(data.locations.map(l => l.adm0_name));
+
   return (
     <Link to={`/map/project/${data.id}`} className="c-project-item">
       <div className="project-advance">
-        <span className="project-name">{data.name}</span>
         <ul className="project-company">
           <li>
             {data.organizations.length && upperFirst(data.organizations[0].name)} {data.organizations.length > 1 && <PlusNumber list={data.organizations} className="-right" />}
           </li>
         </ul>
-        <div className="project-tags-container row collapse">
-          <p className="project-tags column small-12 medium-8">
-            <span className="title">Solution:</span><span> {data.nature_based_solutions.length && upperFirst(data.nature_based_solutions[0].name)} {data.nature_based_solutions.length > 1 && <PlusNumber list={data.nature_based_solutions} className="-right" />}</span>
+        <span className="project-name">{data.name}</span>
+        <div className="project-tags-container">
+          <p className="project-tags">
+            <span className="title">
+              <SvgIcon name="icon-arrow-right-2" className="-smaller" />
+            </span>
+            <span>
+              {data.nature_based_solutions.length && upperFirst(data.nature_based_solutions[0].name)} {data.nature_based_solutions.length > 1 && <PlusNumber list={data.nature_based_solutions} className="-right" />}
+            </span>
           </p>
-          <p className="project-tags column small-12 medium-4">
-            <span className="title">Hazard: </span><span> {data.hazard_types.length && upperFirst(data.hazard_types[0].name)} {data.hazard_types.length > 1 && <PlusNumber list={data.hazard_types} className="-right" />}</span>
+          <p className="project-tags">
+            <span className="title">
+              <SvgIcon name="icon-arrow-right-2" className="-smaller" />
+            </span>
+            <span>
+              {data.hazard_types.length && upperFirst(data.hazard_types[0].name)} {data.hazard_types.length > 1 && <PlusNumber list={data.hazard_types} className="-right" />}
+            </span>
+          </p>
+          <p className="project-tags">
+            <span className="title">
+              <SvgIcon name="icon-arrow-right-2" className="-smaller" />
+            </span>
+            <span>
+              {countries.length && upperFirst(countries[0])} {countries.length > 1 && <PlusNumber list={countries} className="-right" />}
+            </span>
           </p>
         </div>
       </div>
