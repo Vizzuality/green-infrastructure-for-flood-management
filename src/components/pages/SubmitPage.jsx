0bb8ecc87f95ec1f6574426f59079d280ece9500
bitTal <albafjez@gmail.com>
2017-05-08 15:24:07 +0200

    Feedback changes

--- src/components/pages/SubmitPage.jsx
+++ src/components/pages/SubmitPage.jsx
@@ -9,6 +9,7 @@ import { SvgIcon } from 'vizz-components';
 import BtnGroup from 'components/ui/BtnGroup';
 import RadioGroup from 'components/ui/RadioGroup';
 import InputMap from 'components/ui/InputMap';
+import Info from 'components/ui/Info';
 
 
 const defaultValues = {
@@ -29,6 +30,15 @@ const defaultValues = {
   summary: '',
   learn_more: '',
   references: ''
+};
+
+const infoTexts = {
+  organizations: 'Provide the main organization(s) involved in the project, excluding donors.',
+  donors: 'Provide the main donor organization(s).',
+  intervention_type: 'Green: Measures that consist of ecosystems that are naturally present in the area or that can be restored or recreated if they are degraded or have disappeared. Hybrid: Measures that utilize a combination of both green measures and man-made infrastructure measures to simultaneously establish immediate risk reduction while maintaining the valuable role of the relevant ecosystem.',
+  nature_based_solutions: 'Ecosystems are central to nature-based solutions. Indicate the ecosystem(s) your project conserved, restored or created.',
+  primary_benefits_of_interventions: 'Indicate the primary hazard mitigation benefits of the intervention.',
+  co_benefits_of_interventions: 'Indicate the social, environmental, and economic co-benefits.'
 };
 
 export default class SubmitPage extends React.Component {
@@ -143,14 +153,14 @@ export default class SubmitPage extends React.Component {
             <Row>
               <div className="column small-12 medium-8 medium-offset-2">
                 <h1 className="h1 -line">Sumbit your project</h1>
-                <p className="text">Contribute your nature-based project and experiences to The Nature of Risk Reduction database, and join a growing community of practitioners, scientists and donors who are using nature-based approaches to reduce disaster risk.</p>
+                <p className="text">Contribute your nature-based project and experiences to The Nature of Risk Reduction database, and join a growing community of practitioners, scientists and donors using nature-based approaches to reduce disaster risk.</p>
               </div>
             </Row>
 
             <Row>
               <div className="c-form column small-12 medium-8 medium-offset-2">
                 <div className="form">
-                  {/* Name */}
+                  {/* Project Name */}
                   <div className={`form-field ${this.isRequiredOn('name')}`}>
                     <input
                       ref={n => this.name = n}
@@ -158,87 +168,19 @@ export default class SubmitPage extends React.Component {
                       type="text"
                       onBlur={e => this.setFieldValue('name', e.currentTarget.value)}
                     />
-                    <h2 className="label">Name*</h2>
-                  </div>
-
-                  <div className="form-field">
-                    {this.renderLocations()}
-                    <InputMap
-                      inputProps={{ name: 'locations' }}
-                      onAddLocation={this.onAddLocation}
-                    />
-                    <h2 className="label">Location</h2>
-                  </div>
-
-                  {/* Scale */}
-                  <div className="form-field">
-                    <h2 className="label">Scale</h2>
-                    <RadioGroup
-                      name="scale"
-                      options={filtersOptions.scales || []}
-                      selected={filtersOptions.scales && filtersOptions.scales.find(imp => imp.value === scale)}
-                      onChange={value => this.setFieldValue('scale', value)}
-                    />
+                    <h2 className="label">Project Name*</h2>
                   </div>
 
                   {/* Organizations */}
                   <div className="form-field">
                     <Select
-                      name="field"
+                      name="organizations"
                       multi
                       options={filtersOptions.organizations}
                       value={filtersOptions.organizations ? filtersOptions.organizations.filter(opt => organizations.includes(opt.value)) : []}
                       onChange={opts => this.setFieldValue('organizations', opts.map(o => o.value))}
                     />
-                    <h2 className="label">Organizations</h2>
-                  </div>
-
-                  {/* Primary benefits */}
-                  <div className="form-field">
-                    <Select
-                      name="primary_benefits"
-                      multi
-                      options={filtersOptions.primary_benefits}
-                      value={filtersOptions.primary_benefits ? filtersOptions.primary_benefits.filter(opt => primary_benefits_of_interventions.includes(opt.value)) : []}
-                      onChange={opts => this.setFieldValue('primary_benefits_of_interventions', opts.map(o => o.value))}
-                    />
-                    <h2 className="label">Risk reduction benefits</h2>
-                  </div>
-
-                  {/* Co benefits of interventions */}
-                  <div className="form-field">
-                    <Select
-                      name="field"
-                      multi
-                      options={filtersOptions.co_benefits}
-                      value={filtersOptions.co_benefits ? filtersOptions.co_benefits.filter(opt => co_benefits_of_interventions.includes(opt.value)) : []}
-                      onChange={opts => this.setFieldValue('co_benefits_of_interventions', opts.map(o => o.value))}
-                    />
-                    <h2 className="label">Co benefits of interventions</h2>
-                  </div>
-
-                  {/* Nature-based solutions */}
-                  <div className="form-field">
-                    <Select
-                      name="field"
-                      multi
-                      options={filtersOptions.nature_based_solutions}
-                      value={filtersOptions.nature_based_solutions ? filtersOptions.nature_based_solutions.filter(opt => nature_based_solutions.includes(opt.value)) : []}
-                      onChange={opts => this.setFieldValue('nature_based_solutions', opts.map(o => o.value))}
-                    />
-                    <h2 className="label">Nature-based solutions</h2>
-                  </div>
-
-                  {/* Hazard */}
-                  <div className="form-field">
-                    <Select
-                      name="hazard_types"
-                      multi
-                      options={filtersOptions.hazard_types}
-                      value={filtersOptions.hazard_types ? filtersOptions.hazard_types.filter(opt => hazard_types.includes(opt.value)) : []}
-                      onChange={opts => this.setFieldValue('hazard_types', opts.map(o => o.value))}
-                    />
-                    <h2 className="label">Hazard</h2>
+                    <h2 className="label">Organizations*<Info text={infoTexts.organizations} /></h2>
                   </div>
 
                   {/* Donors */}
@@ -250,8 +192,99 @@ export default class SubmitPage extends React.Component {
                       value={filtersOptions.donors ? filtersOptions.donors.filter(opt => donors.includes(opt.value)) : []}
                       onChange={opts => this.setState({ donors: opts.map(o => o.value) })}
                     />
-                    <h2 className="label">Donors</h2>
+                    <h2 className="label">Donors<Info text={infoTexts.donors} /></h2>
                   </div> */}
+
+                  {/* Locations */}
+                  <div className="form-field">
+                    {this.renderLocations()}
+                    <InputMap
+                      inputProps={{ name: 'locations' }}
+                      onAddLocation={this.onAddLocation}
+                    />
+                    <h2 className="label">Locations*</h2>
+                  </div>
+
+                  {/* Scale */}
+                  <div className="form-field">
+                    <h2 className="label">Scale*</h2>
+                    <RadioGroup
+                      name="scale"
+                      options={filtersOptions.scales || []}
+                      selected={filtersOptions.scales && filtersOptions.scales.find(imp => imp.value === scale)}
+                      onChange={value => this.setFieldValue('scale', value)}
+                    />
+                  </div>
+
+                  {/* Implementation status */}
+                  <div className={`form-field ${this.isRequiredOn('implementation_status')}`}>
+                    <h2 className="label">Implementation status*</h2>
+                    <RadioGroup
+                      name="implementation_status"
+                      options={filtersOptions.implementation_statuses || []}
+                      selected={filtersOptions.implementation_statuses && filtersOptions.implementation_statuses.find(imp => imp.value === implementation_status)}
+                      onChange={value => this.setFieldValue('implementation_status', value)}
+                    />
+                  </div>
+
+                  {/* Hazard types */}
+                  <div className="form-field">
+                    <Select
+                      name="hazard_types"
+                      multi
+                      options={filtersOptions.hazard_types}
+                      value={filtersOptions.hazard_types ? filtersOptions.hazard_types.filter(opt => hazard_types.includes(opt.value)) : []}
+                      onChange={opts => this.setFieldValue('hazard_types', opts.map(o => o.value))}
+                    />
+                    <h2 className="label">Hazard types*</h2>
+                  </div>
+
+                  {/* Intervention type */}
+                  <div className="form-field">
+                    <h2 className="label">Intervention type* <Info text={infoTexts.intervention_type} /></h2>
+                    <RadioGroup
+                      name="intervention_type"
+                      options={filtersOptions.intervention_types || []}
+                      selected={filtersOptions.intervention_types && filtersOptions.intervention_types.find(imp => imp.value === intervention_type)}
+                      onChange={value => this.setFieldValue('intervention_type', value)}
+                    />
+                  </div>
+
+                  {/* Nature-based solutions */}
+                  <div className="form-field">
+                    <Select
+                      name="nature_based_solutions"
+                      multi
+                      options={filtersOptions.nature_based_solutions}
+                      value={filtersOptions.nature_based_solutions ? filtersOptions.nature_based_solutions.filter(opt => nature_based_solutions.includes(opt.value)) : []}
+                      onChange={opts => this.setFieldValue('nature_based_solutions', opts.map(o => o.value))}
+                    />
+                    <h2 className="label">Nature-based solutions* <Info text={infoTexts.nature_based_solutions} /></h2>
+                  </div>
+
+                  {/* Primary benefits */}
+                  <div className="form-field">
+                    <Select
+                      name="primary_benefits"
+                      multi
+                      options={filtersOptions.primary_benefits}
+                      value={filtersOptions.primary_benefits ? filtersOptions.primary_benefits.filter(opt => primary_benefits_of_interventions.includes(opt.value)) : []}
+                      onChange={opts => this.setFieldValue('primary_benefits_of_interventions', opts.map(o => o.value))}
+                    />
+                    <h2 className="label">Risk reduction benefits* <Info text={infoTexts.primary_benefits_of_interventions} /></h2>
+                  </div>
+
+                  {/* Co benefits of interventions */}
+                  <div className="form-field">
+                    <Select
+                      name="field"
+                      multi
+                      options={filtersOptions.co_benefits}
+                      value={filtersOptions.co_benefits ? filtersOptions.co_benefits.filter(opt => co_benefits_of_interventions.includes(opt.value)) : []}
+                      onChange={opts => this.setFieldValue('co_benefits_of_interventions', opts.map(o => o.value))}
+                    />
+                    <h2 className="label">Full range of benefits of intervention <Info text={infoTexts.co_benefits_of_interventions} /></h2>
+                  </div>
 
                   {/* Costs */}
                   <div className="form-field costs">
@@ -264,6 +297,17 @@ export default class SubmitPage extends React.Component {
                         onBlur={e => this.setFieldValue('estimated_cost', e.currentTarget.value)}
                       />
                       <h2 className="label">Estimated Cost</h2>
+                    </div>
+
+                    <div className="cost-field -currency">
+                      <Select
+                        name="original_currency"
+                        multi={false}
+                        options={currencyOptions}
+                        value={original_currency || ''}
+                        onChange={opt => this.setFieldValue('original_currency', opt ? opt.value : '')}
+                      />
+                      <h2 className="label">Currency of estimated cost</h2>
                     </div>
 
                     <div className="cost-field">
@@ -287,28 +331,6 @@ export default class SubmitPage extends React.Component {
                       />
                       <h2 className="label">Original currency</h2>
                     </div>
-                  </div>
-
-
-                  {/* Intervention type* */}
-                  <div className="form-field">
-                    <h2 className="label">Intervention type*</h2>
-                    <RadioGroup
-                      name="intervention_type"
-                      options={filtersOptions.intervention_types || []}
-                      selected={filtersOptions.intervention_types && filtersOptions.intervention_types.find(imp => imp.value === intervention_type)}
-                      onChange={value => this.setFieldValue('intervention_type', value)}
-                    />
-                  </div>
-
-                  <div className={`form-field ${this.isRequiredOn('implementation_status')}`}>
-                    <h2 className="label">Implementation status*</h2>
-                    <RadioGroup
-                      name="implementation_status"
-                      options={filtersOptions.implementation_statuses || []}
-                      selected={filtersOptions.implementation_statuses && filtersOptions.implementation_statuses.find(imp => imp.value === implementation_status)}
-                      onChange={value => this.setFieldValue('implementation_status', value)}
-                    />
                   </div>
 
                   {/* Benefit details */}
