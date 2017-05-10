ce9baef1d55cb62da50c1fc9a32d8d9d1607a93c
bitTal <albafjez@gmail.com>
2017-05-10 11:45:20 +0200

    Toggle legend

--- src/components/ui/Legend.jsx
+++ src/components/ui/Legend.jsx
@@ -20,69 +20,81 @@ export default class Legend extends React.Component {
     super(props);
     // Initial state
     this.state = {
-      // checked: this.props.selected.map(opt => opt.value || value) || []
+      open: true
     };
     // BINDINGS
-    // this.onChange = this.onChange.bind(this);
+    this.onClickOpen = this.onClickOpen.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-  }
+  // componentWillReceiveProps(nextProps) {
+  // }
 
   /**
    * UI EVENTS
-   * - onChange
+   * - onClickOpen
   */
 
+  onClickOpen() {
+    this.setState({ open: !this.state.open });
+  }
 
   render() {
+    const classNames = `c-legend ${this.props.className ? this.props.className : ''}${!this.state.open ? ' -hidden' : ''}`;
+
     return (
-      <div className={`c-legend ${this.props.className ? this.props.className : ''}`}>
+      <div className={classNames}>
         <div className="header">
-          <h1 className="title">Legend <SvgIcon name="icon-arrow-down-2" className="-medium" /></h1>
+          <h1 className="title">
+            Legend
+            <button onClick={this.onClickOpen}>
+              <SvgIcon name="icon-arrow-down-2" className="-medium" />
+            </button>
+          </h1>
         </div>
 
-        <div className="clusters">
-          <ul className="list">
-            <li className="legend-item">
-              <div className="icon">
-                <span className="project-icon" />
-              </div>
-              <p>Projects</p>
-            </li>
-            <li className="legend-item">
-              <div className="icon">
-                <span className="centroid-icon" />
-              </div>
-              <p>Locations centroid</p>
-            </li>
-            <li className="legend-item">
-              <div className="icon">
-                <span className="location-icon" />
-              </div>
-              <p>Current project location </p>
-            </li>
-          </ul>
-        </div>
-
-        <div className={`layers ${this.props.layerActive ? '-active' : ''}`}>
-          <div className="layers-header">
-            <Switch
-              // onClick={this.onChangeCostSwitch}
-              // on={!this.state.cost.disabled}
-              className="c-switch"
-            />
-            Potential river and coastal flood inundation
+        <div className="content">
+          <div className="clusters">
+            <ul className="list">
+              <li className="legend-item">
+                <div className="icon">
+                  <span className="project-icon" />
+                </div>
+                <p>Projects</p>
+              </li>
+              <li className="legend-item">
+                <div className="icon">
+                  <span className="centroid-icon" />
+                </div>
+                <p>Locations centroid</p>
+              </li>
+              <li className="legend-item">
+                <div className="icon">
+                  <span className="location-icon" />
+                </div>
+                <p>Current project location </p>
+              </li>
+            </ul>
           </div>
-          <div className="layer-spec">
-            <div className="legend-colors">
-              {legendConfig.map((l, i) => (
-                <span key={i} className="square" style={{ background: l.color }} data-value={l.value} />
-              ))}
-              <span className="mark" data-value="M" />
+
+          <div className={`layers ${this.props.layerActive ? '-active' : ''}`}>
+            <div className="layers-header">
+              <Switch
+                // onClick={this.onChangeCostSwitch}
+                // on={!this.state.cost.disabled}
+                className="c-switch"
+              />
+              Potential river and coastal flood inundation
             </div>
-            <div className="arrow">
-              <SvgIcon name="icon-large-arrow" className="" />
+            <div className="layer-spec">
+              <div className="legend-colors">
+                {legendConfig.map((l, i) => (
+                  <span key={i} className="square" style={{ background: l.color }} data-value={l.value} />
+                ))}
+                <span className="mark" data-value="M" />
+              </div>
+              <div className="arrow">
+                <SvgIcon name="icon-large-arrow" className="" />
+              </div>
             </div>
           </div>
         </div>
