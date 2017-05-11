c29826c78c36f62c58469cc742401a9bd475a8ad
bitTal <albafjez@gmail.com>
2017-05-11 17:21:31 +0200

    Upload image styles

--- src/components/ui/ImageUpload.jsx
+++ src/components/ui/ImageUpload.jsx
@@ -8,48 +8,49 @@ export default class ImageUpload extends React.Component {
 
     return (
       <section className="c-image-upload">
-        <Dropzone
-          name="image"
-          className="dropzone"
-          maxSize={3145728}
-          accept={accept}
-          onDrop={(acc, rej) => onDrop(acc, rej)}
-          multiple={false}
-          activeClassName="active-image"
-        >
-          <SvgIcon name="icon-plus" className="add -huge" />
-          <p className="notice">Add photo</p>
-        </Dropzone>
-        <aside className="file-selected">
-          {accepted &&
-            <div className="files -accepted">
-              <div>
-                <div className="image-uploaded" style={{ backgroundImage: `url(${accepted.attachment})` }}>
-                  <div className="viel" />
+        <div className="upload">
+          <Dropzone
+            name="image"
+            className="dropzone"
+            maxSize={3145728}
+            accept={accept}
+            onDrop={(acc, rej) => onDrop(acc, rej)}
+            multiple={false}
+            activeClassName="active-image"
+          >
+            <SvgIcon name="icon-plus" className="add -huge" />
+            <p className="notice">Add photo</p>
+          </Dropzone>
+          <aside className="file-selected">
+            {accepted &&
+              <div className="files -accepted">
+                <div>
+                  <div className="image-uploaded" style={{ backgroundImage: `url(${accepted.attachment})` }}>
+                    <div className="viel" />
+                    <button className="remove" onClick={() => onRemoveFile()}>
+                      <SvgIcon name="icon-cross" className="-huge" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            }
+
+            {showRejected && rejected &&
+              <div className="files -rejected">
+                <h2>File rejected</h2>
+                <div>
+                  <p>{rejected.name} - {rejected.size} bytes</p>
                   <button className="remove" onClick={() => onRemoveFile()}>
                     <SvgIcon name="icon-cross" className="-huge" />
                   </button>
-                  {/* <p>{accepted.name} - {accepted.size} bytes</p> */}
                 </div>
               </div>
-            </div>
-          }
-
-          {showRejected && rejected &&
-            <div className="files -rejected">
-              <h2>File rejected</h2>
-              <div>
-                <div className="image-uploaded" style={{ backgroundImage: `url(${rejected.attachment})` }} />
-                <p>{rejected.name} - {rejected.size} bytes</p>
-                <button className="remove" onClick={() => onRemoveFile()}>
-                  <SvgIcon name="icon-cross" className="-huge" />
-                </button>
-              </div>
-            </div>
-          }
-
-          {!showRejected && rejected && <p>The file has been rejected</p>}
-        </aside>
+            }
+          </aside>
+        </div>
+        <div className="upload-error">
+          {!showRejected && rejected && <p className="error">The file has been rejected</p>}
+        </div>
       </section>
     );
   }
