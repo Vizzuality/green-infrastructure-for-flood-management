5b941740fc9369e732585ea30dcc2465974c605c
bitTal <albafjez@gmail.com>
2017-05-12 14:14:00 +0200

    Responsive guidance principles

--- src/components/pages/guidance/Principles.jsx
+++ src/components/pages/guidance/Principles.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'rc-collapse/assets/index.css';
 import Collapse, { Panel } from 'rc-collapse';
 import { SvgIcon } from 'vizz-components';
+import { isDevice } from 'utils/general';
 import { Row } from 'components/ui/Grid';
 import { principle1, principle2, principle3, principle4, principle5 } from 'constants/guidance';
 
@@ -31,16 +32,18 @@ export default class Principles extends React.Component {
         {principles.map(p => (
           <Panel
             header={(
-              <h2 className="pp-title">
+              <div className="pp-header">
                 <button className="show-more" onClick={() => this.onClick(p.number)}>
                   {(activeKey.length && activeKey[0] !== p.number) || !activeKey.length ?
                     <SvgIcon name="icon-plus" className="more -small" /> :
                     <SvgIcon name="icon-minus" className="more -small" />
                   }
                 </button>
-                <span className="pp-number">Principle {p.number}</span>
-                <span>{p.title}</span>
-              </h2>
+                <h2 className="pp-title">
+                  <span className="pp-number">Principle {p.number}</span>
+                  <span>{p.title}</span>
+                </h2>
+              </div>
             )}
             key={p.number}
             className={activeKey.length && activeKey[0] === p.number ? '-active' : ''}
@@ -62,7 +65,7 @@ export default class Principles extends React.Component {
               <h3 className="tag">Guidance</h3>
               <h1 className="title">Guiding principles</h1>
               <p className="text">Recent years have yielded numerous pilot projects and technical reports on the implementation of nature-based solutions for flood risk management. The following basic principles can be used to guide project development in the field. The main purpose of these principles is to build on best practices and prevent common pitfalls in the use of nature-based solutions. As such, these principles are meant as guidance for project development and initiation and not as a specific design manual. For more detailed information on design we refer to existing technical guidelines (including NOAA and Ecoshape[]) and guidelines under development (USACE).</p>
-              <span className="line"></span>
+              <span className="line" />
             </div>
           </Row>
         </section>
