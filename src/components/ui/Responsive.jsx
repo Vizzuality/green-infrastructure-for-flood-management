c275ba94bfc953210dc2bd40ea6c5246946dd75f
Clara Linos <clara.linos@gmail.com>
2017-05-25 18:55:06 +0200

    header image changed for river one

--- src/components/ui/Responsive.jsx
+++ src/components/ui/Responsive.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import MediaQuery from 'react-responsive';
 
 const BREAKPOINT_MOBILE = 640;
+const BREAKPOINT_TABLET = 1024;
 
 export default function OnlyOn(props) {
   const breakpoints = {
-    mobile: `(max-width: ${BREAKPOINT_MOBILE}px)`,
-    desktop: `(min-width: ${BREAKPOINT_MOBILE + 1}px)`
+    mobile: `(max-width: ${BREAKPOINT_TABLET}px)`,
+    desktop: `(min-width: ${BREAKPOINT_TABLET + 1}px)`
   };
   return (
     <MediaQuery query={breakpoints[props.device]}>
