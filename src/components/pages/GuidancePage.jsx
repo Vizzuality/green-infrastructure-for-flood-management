a33682cc9309e273cfd8484bf4966dd0abc8dad7
bitTal <albafjez@gmail.com>
2017-10-23 15:08:23 +0200

    Add guidance pd

--- src/components/pages/GuidancePage.jsx
+++ src/components/pages/GuidancePage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.scss';
 
+import { SvgIcon } from 'vizz-components';
 import Introduction from 'components/pages/guidance/Introduction';
 import Principles from 'components/pages/guidance/Principles';
 import Implementation from 'components/pages/guidance/Implementation';
+
 
 export default class GuidancePage extends React.Component {
   constructor(props) {
@@ -19,7 +21,13 @@ export default class GuidancePage extends React.Component {
           <div className="l-app-wrapper">
             <div className="section-wrapper">
               <h1 className="h1 -secondary -big">Guidance</h1>
-              <p className="intro">Implementing nature-based flood protection</p>
+              <div className="intro-container">
+                <p className="intro">Implementing nature-based flood protection</p>
+                <a className="c-btn -download" href="/files/Implementing_nature-based_flood_protection_Principles_and_implementation.pdf" download>
+                  Download pdf
+                  <SvgIcon name="icon-download-white" />
+                </a>
+              </div>
             </div>
           </div>
         </section>
