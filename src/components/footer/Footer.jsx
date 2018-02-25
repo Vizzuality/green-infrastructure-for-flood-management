c1eb9fbc3f532a5e4cb3be209822839dd3a6d276
David Inga <david.inga@vizzuality.com>
2018-02-25 13:26:23 +0100

    added copy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
   return (
     <footer className="c-footer" role="banner">
       <div className="footer-content l-app-wrapper">
+        <div className="copy">©2017 The World Bank</div>
         <Nav className="c-nav -footer" links={footerLinks} />
       </div>
     </footer>
